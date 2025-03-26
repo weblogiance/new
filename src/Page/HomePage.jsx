46f334adc65e814b19512e159223b2f66a2f3c40
weblogiance <ankur.weblogiance@gmail.com>
2025-03-26 10:31:46 +0530

    new backend idia.

--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -41,7 +41,7 @@ const HomePage = () => {
             <CollageProject/>
             <Buttonsection/>
             <Carthome/>
-            <Backendsite/>
+            <Backendsite/>  
         </div>
     </div>
   )
